db5039d58c8ba79628550ad997c064cbf1ad76b7
anthonyc <colavitoanthony@gmail.com>
2025-03-13 13:02:45 -0400

    Expanded hypo_workers dataframe to include ages

--- programs/create_workers.R
+++ programs/create_workers.R
@@ -5,16 +5,16 @@ setwd("/Users/anthony/Desktop/SSMBAr")
 birth_cohorts <- seq(1945, 2030, by = 5)
 sexes <- c("male", "female")
 earn_types <- c("none","verylow","low","med","high","max")
-ages <- 22:100
+ages <- 21:100
 
 hypo_workers <- expand_grid(
   birth_cohort = birth_cohorts,
   sex = sexes,
-  earn_type = earn_types
+  earn_type = earn_types,
+  age = ages
 ) %>% 
   mutate(
-  worker = paste0("w_",birth_cohort,"_",substr(sex,1,1),"_",earn_type),
-  id = row_number()
+  worker = paste0("w_",birth_cohort,"_",substr(sex,1,1),"_",earn_type)
 ) 
 
 #Load Assumptions
