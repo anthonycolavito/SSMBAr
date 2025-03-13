de20304fab8b5fbda52010ddfeb3e756bc5caaf7
anthonyc <colavitoanthony@gmail.com>
2025-03-13 17:30:22 -0400

    Removed extraneous code from create_workers and restructured it slightly

--- programs/create_workers.R
+++ programs/create_workers.R
@@ -11,23 +11,21 @@ prepare_assumptions()
 birth_cohorts <- seq(1945, 2030, by = 5)
 sexes <- c("male", "female")
 earn_types <- c("none","verylow","low","medium","high","max")
-ages <- 21:100
 
-#Create hypothetical workers dataset
+#Create hypothetical worker types dataset
 hypo_workers <- expand_grid(
   birth_cohort = birth_cohorts,
   sex = sexes,
-  earn_type = earn_types,
-  age = ages
+  earn_type = earn_types
 ) %>% 
   mutate(
   worker = paste0("w_",birth_cohort,"_",substr(sex,1,1),"_",earn_type),
-  year = age + birth_cohorts
+  id = row_number()
 ) 
 
-#Load Assumptions
-assumptions <- readRDS("./data/assumptions.RDS")
-scaled_earnings <- read_csv("./data/2024 earnings scaling factors.csv")
+for(worker in hypo_workers){
+  
+}
 
 #Merge scaled earnings assumptions 
 hypo_workers <- merge(hypo_workers, scaled_earnings, by="age", all.x = TRUE)
