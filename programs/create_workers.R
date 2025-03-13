0e3b5606be3d92a36df4ee00f96ca454977117f1
anthonyc <colavitoanthony@gmail.com>
2025-03-13 13:07:27 -0400

    First pass at merging scaled earnings to hypothetical workers

--- programs/create_workers.R
+++ programs/create_workers.R
@@ -4,9 +4,10 @@ setwd("/Users/anthony/Desktop/SSMBAr")
 
 birth_cohorts <- seq(1945, 2030, by = 5)
 sexes <- c("male", "female")
-earn_types <- c("none","verylow","low","med","high","max")
+earn_types <- c("none","verylow","low","medium","high","max")
 ages <- 21:100
 
+#Create hypothetical workers dataset
 hypo_workers <- expand_grid(
   birth_cohort = birth_cohorts,
   sex = sexes,
@@ -14,10 +15,13 @@ hypo_workers <- expand_grid(
   age = ages
 ) %>% 
   mutate(
-  worker = paste0("w_",birth_cohort,"_",substr(sex,1,1),"_",earn_type)
+  worker = paste0("w_",birth_cohort,"_",substr(sex,1,1),"_",earn_type),
+  year = age + birth_cohorts
 ) 
 
 #Load Assumptions
 assumptions <- readRDS("./data/assumptions.RDS")
 scaled_earnings <- read_csv("./data/2024 earnings scaling factors.csv")
 
+#Merge scaled earnings assumptions 
+hypo_workers <- merge(hypo_workers, scaled_earnings, by="age", all.x = TRUE)
